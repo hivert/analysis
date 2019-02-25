95fa92412b00f2c87cc673394f5b7660de723f5d
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2019-02-25 22:46:21 +0900

    prove squeeze for sequences from squeeze for functions

--- sequences.v
+++ sequences.v
@@ -51,13 +51,11 @@ rewrite addo'.
 done.
 Qed.
 
-Lemma squeeze_fun (T : Type) (f g h : T -> R) (a : filter_on T) (l : R) :
-  f @ a --> l ->
-  h @ a --> l ->
-  (\forall x \near a, f x <= g x <= h x) ->
-  g @ a --> l.
+Lemma squeeze (T : Type) (f g h : T -> R) (a : filter_on T) :
+  (\forall x \near a, f x <= g x <= h x) -> forall (l : R),
+  f @ a --> l -> h @ a --> l -> g @ a --> l.
 Proof.
-move=> fal hal afgh; apply/flim_locally => _/posnumP[/= e].
+move=> afgh l fal hal; apply/flim_locally => _/posnumP[/= e].
 rewrite near_map; near=> x.
 rewrite /ball /= /AbsRing_ball /= ltr_norml; apply/andP; split.
 - rewrite ltr_oppl opprB (@ler_lt_trans _ (h x - l)) //.
@@ -76,32 +74,18 @@ rewrite /ball /= /AbsRing_ball /= ltr_norml; apply/andP; split.
     by rewrite near_map.
 Grab Existential Variables. all: end_near. Qed.
 
-Lemma squeeze (u_ v_ w_ : (R^o) ^nat) l :
-  (exists N, forall n, (n >= N)%nat -> u_ n <= v_ n <= w_ n) ->
+Lemma squeeze_sequence (u_ v_ w_ : (R^o) ^nat) l :
+  (\forall n \near \oo, u_ n <= v_ n <= w_ n) ->
   cvg u_ -> lim u_ = l ->
   cvg w_ -> lim w_ = l ->
   cvg v_ /\ lim v_ = l.
 Proof.
-case=> N uvw cvgu ul cvgw wl.
+move=> uvw cvgu ul cvgw wl.
 suff vol : v_ @ \oo --> l.
   by split; [exact/(@cvgP _ _ (v_ @ \oo) l) | exact/flim_map_lim].
-apply/flim_normP => _/posnumP[e].
-near_simpl; near=> N0; rewrite ltr_norml; apply/andP; split.
-- rewrite ltr_oppl opprB (@ler_lt_trans _ (w_ N0 - l)) //.
-  + rewrite ler_sub //.
-    have /(_ _) /andP[|//] := @uvw N0.
-    by near: N0; by exists N.
-  + rewrite (@ler_lt_trans _ `|[w_ N0 - l]|) // ?real_ler_norm // ?num_real //.
-    rewrite normmB.
-    near: N0; move/flim_normP : cvgw; rewrite wl; exact.
-- rewrite (@ler_lt_trans _ (l - u_ N0)) //.
-  + rewrite ler_sub //.
-    have /(_ _) /andP[|//] := @uvw N0.
-    by near: N0; by exists N.
-  + rewrite (@ler_lt_trans _ `|[u_ N0 - l]|) //.
-      by rewrite -normmB real_ler_norm // num_real.
-    rewrite normmB.
-    near: N0; move/flim_normP : cvgu; rewrite ul; exact.
-Grab Existential Variables. all: end_near. Qed.
+apply: (@squeeze _ _ _ _ _ uvw l).
+- case/cvg_ex : cvgu => /= x ux; by rewrite -ul (flim_lim ux).
+- case/cvg_ex : cvgw => /= x wx; by rewrite -wl (flim_lim wx).
+Qed.
 
 End sequences.
