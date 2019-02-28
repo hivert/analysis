c222edb88766f0993303cd21ba231179eb0f5c94
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2019-02-28 16:28:43 +0900

    convergent sequences are bounded

--- sequences.v
+++ sequences.v
@@ -12,41 +12,7 @@ Import GRing.Theory Num.Def Num.Theory.
 
 Local Open Scope classical_set_scope.
 
-Require Import derive.
-
-Lemma running_example (f g h : R^o -> R^o) x :
- derivable f x 1 -> derivable g x 1 -> derivable h x 1 ->
- is_derive x 1 (f + g * h) (f^`() x + g^`() x * h x + g x * h^`() x).
-Proof.
-move=> /derivableP Hf /derivableP Hg /derivableP Hh.
-apply: is_derive_eq.
-rewrite addrAC (mulrC _ (h x)) -addrA.
-by rewrite !derive1E.
-Qed.
-
-Notation "[ 'd' x = g # p ]" := (projT1 (existT (fun f => is_diff x f g) _ p))
-  (at level 0, x at next level, format "[ 'd'  x  =  g  #  p ]").
-
-Definition f0 (g : R^o -> R^o) (x : R^o) : R^o -> R^o := fun y => g (y - x).
-
-Lemma mkdiff (g : {linear R^o -> R^o}) (x : R^o) : continuous g ->
-  is_diff x (f0 g x) g.
-Proof.
-move=> cg.
-set F0 := f0 g x.
-suff H : forall h : R^o, F0 (h + x) = F0 x + g h +o_(h \near 0 : R^o) h.
-  have df0 : 'd F0 x = g :> (R^o -> R^o).
-    apply diff_unique => //.
-    by rewrite funeqE.
-  apply: DiffDef => //.
-  apply/diff_locallyxP; split => /=; first by rewrite df0.
-  by move=> h; rewrite H df0.
-apply: eqaddoEx => h.
-rewrite /F0 /f0 addrK subrr linear0 add0r.
-apply/eqP; rewrite addrC -subr_eq subrr; apply/eqP.
-rewrite littleoE; last exact: littleo0_subproof.
-by [].
-Qed.
+(* scratch file *)
 
 Definition sequence R := nat -> R.
 Notation "R ^nat" := (sequence R) (at level 0).
@@ -67,7 +33,6 @@ Lemma addo' (K : absRingType) (T : Type) (V W : normedModType K) (F : filter_on 
   [o_F e of f] + [o_F' e of g] =o_F e.
 Proof. exact: addo. Qed.
 
-
 Lemma lim_add3sequence (u_ v_ w_ : (R^o) ^nat) : cvg u_ -> cvg v_ -> cvg w_ ->
   lim (u_ + v_ + w_) = lim u_ + lim v_ +  lim w_.
 Proof.
@@ -86,6 +51,70 @@ congr (_ + _ + _ + _).
 rewrite addo'.
 done.
 Qed.
+
+Lemma dvgP (u_ : (R^o) ^nat) : u_ --> +oo <-> forall A : posreal, \forall n \near \oo, A <= u_ n.
+Proof.
+split.
+  move=> ulim A; rewrite -(near_map u_ \oo (<=%R A)).
+  by apply: ulim; apply: locally_pinfty_ge.
+  (* move=> ulim A.
+  move/filter_fromP : ulim.
+  move/(_ (fun x => A <= x)) => /= ulim.
+  suff /ulim : filter_from [filter of +oo] id (fun x : R^o => A <= x) by [].
+  eexists; last by move=> ?; exact.
+  by exists A%:num => ? /ltrW. *)
+move=> /(_ (PosNum _)) u_ge X [A AX].
+rewrite near_simpl [\forall x \near _, X x](near_map u_ \oo).
+near=> x; apply: AX; rewrite (@ltr_le_trans _ ((maxr 0 A) +1)) //.
+  by rewrite ltr_spaddr// ler_maxr lerr orbT.
+by near: x; apply: u_ge; rewrite ltr_spaddr// ler_maxr lerr.
+Grab Existential Variables. all: end_near. Qed.
+
+(* lemma available in the branch uniform-entourages *)
+Lemma bigmaxrD1 (I : finType) j (P : pred I) (F : I -> R) x :
+  P j -> \big[maxr/x]_(i | P i) F i
+    = maxr (F j) (\big[maxr/x]_(i | P i && (i != j)) F i).
+Proof.
+Admitted.
+
+Lemma ler_bigmax_cond (I : finType) (P : pred I) (F : _ -> R) i0 :
+  P i0 -> (forall x, 0 <= F x) -> F i0 <= \big[Num.max/0]_(i | P i) F i.
+Proof.
+move=> Pi0 F0.
+rewrite (@bigmaxrD1 _ i0) //=.
+by rewrite ler_maxr lerr.
+Qed.
+
+Lemma cvg_bound (u_ : (R^o) ^nat) : cvg u_ -> exists M, forall n, `|u_ n| <= M.
+Proof.
+move=> cu; set l := lim u_.
+have u1l : \forall n \near \oo, `|u_ n| <= 1 + `|l|.
+  have : \forall n \near \oo, `|l - u_ n| < 1 by move/flim_normP : cu; apply.
+  apply: filter_app.
+  near=> n.
+  move=> lu1.
+  have ul := ler_distm_dist (u_ n) l.
+  rewrite -ler_sub_addr.
+  apply/ltrW.
+  by rewrite (ler_lt_trans _ lu1) // absrB (ler_trans _ ul) // ler_norm.
+case: u1l => x _ xu.
+near \oo => N.
+set M := Num.max (\big[maxr/0]_(0 <= i < N.+1) `|u_ i|) (1 + `|l|).
+exists M.
+rewrite {}/M.
+move=> n.
+rewrite ler_maxr.
+case/boolP : (n <= N)%nat => [|].
+  rewrite -ltnS => nN.
+  rewrite big_mkord.
+  by rewrite (@ler_bigmax_cond [finType of 'I_N.+1] _ _ (Ordinal nN)).
+rewrite -ltnNge => Nn.
+apply/orP; right.
+apply/xu/ltnW.
+apply: leq_ltn_trans Nn.
+near: N.
+exists x => //.
+Grab Existential Variables. all: end_near. Qed.
 
 Lemma squeeze (T : Type) (f g h : T -> R) (a : filter_on T) :
   (\forall x \near a, f x <= g x <= h x) -> forall (l : R),
@@ -124,32 +153,63 @@ apply: (@squeeze _ _ _ _ _ uvw l).
 - case/cvg_ex : cvgw => /= x wx; by rewrite -wl (flim_lim wx).
 Qed.
 
-Lemma dvgP' (u_ : (R^o) ^nat) : (forall A : posreal, \forall n \near \oo, A <= u_ n) -> u_ --> +oo.
-Proof.
-move=> H.
-Admitted.
-
-Lemma dvgP (u_ : (R^o) ^nat) : u_ --> +oo -> forall A : posreal, \forall n \near \oo, A <= u_ n.
-Proof.
-move=> ulim A; rewrite -(near_map u_ \oo (<=%R A)).
-by apply: ulim; apply: locally_pinfty_ge.
-(* move=> ulim A.
-move/filter_fromP : ulim.
-move/(_ (fun x => A <= x)) => /= ulim.
-suff /ulim : filter_from [filter of +oo] id (fun x : R^o => A <= x) by [].
-eexists; last by move=> ?; exact.
-by exists A%:num => ? /ltrW. *)
-Qed.
-
 Lemma dvg_seq (u_ v_ : (R^o) ^nat) : (\forall n \near \oo, u_ n <= v_ n) ->
   u_ --> +oo -> v_ --> +oo.
 Proof.
 move=> uv.
 move/dvgP => dvgu.
-apply/dvgP' => A.
+apply/dvgP => A.
 near=> n.
 have uA := dvgu A.
 rewrite (@ler_trans _ (u_ n)) //; by near: n.
 Grab Existential Variables. all: end_near. Qed.
 
 End sequences.
+
+Require Import derive.
+
+(* scratch *)
+Section rewriting_differential.
+
+Let running_example (f g h : R^o -> R^o) x :
+ derivable f x 1 -> derivable g x 1 -> derivable h x 1 ->
+ is_derive x 1 (f + g * h) (f^`() x + g^`() x * h x + g x * h^`() x).
+Proof.
+move=> /derivableP Hf /derivableP Hg /derivableP Hh.
+apply: is_derive_eq.
+rewrite addrAC (mulrC _ (h x)) -addrA.
+by rewrite !derive1E.
+Qed.
+
+Definition f0 (g : R^o -> R^o) (x : R^o) : R^o -> R^o := fun y => g (y - x).
+
+Lemma diff_subproof (g : {linear R^o -> R^o}) (x : R^o) : continuous g ->
+  is_diff x (f0 g x) g.
+Proof.
+move=> cg.
+set F0 := f0 g x.
+suff H : forall h : R^o, F0 (h + x) = F0 x + g h +o_(h \near 0 : R^o) h.
+  have df0 : 'd F0 x = g :> (R^o -> R^o).
+    apply diff_unique => //.
+    by rewrite funeqE.
+  apply: DiffDef => //.
+  apply/diff_locallyxP; split => /=; first by rewrite df0.
+  by move=> h; rewrite H df0.
+apply: eqaddoEx => h.
+rewrite /F0 /f0 addrK subrr linear0 add0r.
+apply/eqP; rewrite addrC -subr_eq subrr; apply/eqP.
+rewrite littleoE; last exact: littleo0_subproof.
+by [].
+Qed.
+
+Local Notation "[ 'd' x = g # p ]" := (projT1 (existT (fun f => is_diff x f g) _ p))
+  (at level 0, x at next level, format "[ 'd'  x  =  g  #  p ]").
+
+Section diff_type.
+Context {K : absRingType} {V W : normedModType K}.
+Structure diff_type (diff : V -> W) x := DiffType {
+  diff_fun : V -> W ;
+  _ : is_diff x diff_fun diff }.
+End diff_type.
+
+End rewriting_differential.
